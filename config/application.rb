bb38848df5d9cadc0097d64c14417e86eb416759
PliegerNicolas <plieger05@gmail.com>
2022-04-29 20:10:49 +0200

    Preparing language modification addition via I18n

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,9 @@ module UniqueLatitudeV3
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.i18n.available_locales = [:en, :fr]
+    config.i18n.default_locale = :fr
+    config.i18n.fallbacks = true
   end
 end
