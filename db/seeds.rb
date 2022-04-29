2ffeea18814c7ef46f17c4793b18f18d7404ca00
PliegerNicolas <plieger05@gmail.com>
2022-04-29 12:56:43 +0200

    Adding seeds => testing with false data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,62 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+users = User.create([
+    {
+      username: "user",
+      email: "email1@example.com",
+      password: "password",
+      password_confirmation: "password",
+      role: 0
+    },
+    {
+      username: "admin",
+      email: "email2@example.com",
+      password: "password",
+      password_confirmation: "password",
+      role: 2
+    }
+  ])
+
+projects = Project.create([
+  {
+    title: "Title-1",
+    subject: "A subject",
+    category: 0,
+    user: User.last
+  },
+  {
+    title: "Title-2",
+    subject: "A subject 2",
+    category: 1,
+    user: User.last
+  },
+  {
+    title: "Title-3",
+    subject: "A subject 3",
+    category: 2,
+    user: User.last
+  }
+])
+
+visual_media = VisualMedium.create([
+  {
+    title: "Title-1",
+    url: "http://www.rand-link-1.com",
+    media: 0,
+    project: Project.first
+  },
+  {
+    title: "Title-2",
+    url: "http://www.rand-link-2.com",
+    media: 1,
+    project: Project.first
+  },
+  {
+    title: "Title-3",
+    url: "http://www.rand-link-3.com",
+    media: 1,
+    project: Project.last
+  }
+])
