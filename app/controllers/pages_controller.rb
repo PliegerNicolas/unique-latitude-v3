7d61b94e2aabda72d471aff849e02e8a61a91d99
PliegerNicolas <plieger05@gmail.com>
2022-04-29 19:52:12 +0200

    Randomise 3 projects in index and display them.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   
   def index
-    #@projects = Project.find([*1..Project.count].sample(3))
+    @projects = Project.find([*1..Project.count].sample(3))
   end
 end
