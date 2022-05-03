493142bc658ef752b592162198b35bc60d841f0d
PliegerNicolas <plieger05@gmail.com>
2022-05-03 22:24:16 +0200

    Removing old file after vscode undo fail

--- app/javascript/controllers/search_form_controller.js
+++ app/javascript/controllers/search_form_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="search-form"
 export default class extends Controller {
-  search() {
+  search(event) {
     clearTimeout(this.timeout);
     this.timeout = setTimeout(() => {
       this.element.requestSubmit();
