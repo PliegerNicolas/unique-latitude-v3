4b8af8b50fe87dcc06b3b202fb88731bde42c7a4
PliegerNicolas <plieger05@gmail.com>
2022-05-06 18:13:56 +0200

    Revamp index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'pages#index'
 
+  devise_for :users
+
   resources :projects do
     resources :media
   end
@@ -9,8 +11,7 @@ Rails.application.routes.draw do
   get 'users/:id/promote', to: 'users#promote', as: :promote
   get 'users/:id/demote', to: 'users#demote', as: :demote
   get 'projects/:id/change-status', to: 'projects#change_status', as: :project_change_status
-
-  devise_for :users
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
