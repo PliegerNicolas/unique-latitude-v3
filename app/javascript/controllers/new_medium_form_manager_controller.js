8d1e61693b6b70541d3bfb07517e539bef82a34b
PliegerNicolas <plieger05@gmail.com>
2022-05-20 17:33:54 +0200

    stimulus controller for medium management. Working for image file.

--- app/javascript/controllers/new_medium_form_manager_controller.js
+++ app/javascript/controllers/new_medium_form_manager_controller.js
@@ -4,63 +4,56 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = [ "textField", "fileField", "preview", "fileCancelButton"]
 
-  // -- Actions --
+// Connectors method
 
   connect() {
-    this.displayImagePreview();
-    this.doFileCancelButton();
+    let default_preview_image_src = this.preexisting_image_src(); // Get source for medium-contained-image on edit if exists.
+    this.doFileCancelButton(default_preview_image_src);
     this.disableFields();
+    this.displayImagePreview(default_preview_image_src);
   }
 
-  fileFieldManager() {
-    this.displayImagePreview();
-    this.doFileCancelButton();
+  onTextFieldUpdate() {
     this.disableFields();
+    this.displayImagePreview();
   }
 
-  textFieldManager() {
+  onFileFieldUpdate() {
+    let default_preview_image_src = this.preexisting_image_src(); // Get source for medium-contained-image on edit if exists.
+    this.doFileCancelButton(default_preview_image_src);
     this.disableFields();
+    this.displayImagePreview(default_preview_image_src);
   }
 
   reset() {
     this.element.reset();
-    this.displayImagePreview();
-    this.doFileCancelButton();
+    let default_preview_image_src = this.preexisting_image_src(); // Get source for medium-contained-image on edit if exists.
+    this.doFileCancelButton(default_preview_image_src);
     this.disableFields();
-   }
-
-  // -- Functions --
-
-  // disable file field if text field used or inverse
-  disableFields() {
-    let text_field = this.text
-    let file_field = this.file
-
-    // if text_field contains something, disable & clean file_field
-    if (text_field.value !== '') {
-      file_field.disabled = true;
-      file_field.value = '';
-    } else {
-      file_field.disabled = false;
-    }
-
-    // if file_field contains something, disable & clean text_field
-    if (file_field.value !== '') {
-      text_field.disabled = true;
-      text_field.value = '';
-    } else {
-      text_field.disabled = false;
-    }
+    this.disableImagePreview(default_preview_image_src);
   }
-  
+
+
+
+// Functions
+
+  // Get source for medium-contained-image on edit if exists.
+  preexisting_image_src() {
+    let preview_img = this.preview;
+    if(preview_img.id == 'medium-contained-img') {
+     return preview_img.src;
+    }
+    return;
+  }
+
   // Display & give function to file cancel button when file selected.
-  doFileCancelButton() {
+  doFileCancelButton(default_preview_image_src) {
     if (this.file.value !== '') {
       this.fileCancelButton.style.display = 'block';
       this.fileCancelButton.addEventListener('click', () => {
         this.file.value = '';
-        this.disableImagePreview();
         this.disableFields();
+        this.disableImagePreview(default_preview_image_src);
         this.fileCancelButton.style.display = 'none';
       });
     } else {
@@ -68,49 +61,68 @@ export default class extends Controller {
     }
   }
 
-  // Disable a preview of selected local image
-  displayImagePreview() {
-    let file = this.fileFieldTarget;
-    let img = this.previewTarget;
+  // disable file field if text field used or inverse
+  disableFields() {
+    let text_field = this.text
+    let file_field = this.file
 
-    if (file.value !== '' || img.id == 'medium-contained-img') {
+    if (text_field.value !== '') { // if text_field contains something, disable & clean file_field
+      file_field.disabled = true;
+      file_field.value = '';
+    } else {
+      file_field.disabled = false;
+    }
+
+    if (file_field.value !== '') { // if file_field contains something, disable & clean text_field
+      text_field.disabled = true;
+      text_field.value = '';
+    } else {
+      text_field.disabled = false;
+    }
+  }
+
+  // Disable a preview of selected local image
+  displayImagePreview(default_preview_image_src) {
+    let file = this.file;
+    let img = this.preview;
+
+    if(file.value) { // Display local image if file selected.
       let reader = new FileReader();
       reader.onload = function() {
         img.src = reader.result;
       }
       reader.readAsDataURL(file.files[0]);
-      img.style.display = "block";  
-    } else {
-      img.style.display = "none";      
+      img.style.display = "block";        
     }
   }
 
-  // Hides and cleans the local image preview
-  disableImagePreview() {
-    let img = this.previewTarget;
-    img.src = '';
-    img.style.display = "none";  
+  // Replaces by default or removes preview image
+  disableImagePreview(default_preview_image_src) {
+    let img = this.preview;
+    if(default_preview_image_src) {
+      img.src = default_preview_image_src
+      img.style.display = "block";
+    } else {
+      img.src = '';
+      img.style.display = "none";
+    }  
   }
 
-  // -- Getters --
+
+
+
+// Getters
+
+  get preview() {
+    return this.previewTarget;
+  }
 
   get text() {
-    // get value in text_field
     return this.textFieldTarget
   }
 
   get file() {
-    // get value in file_field
     return this.fileFieldTarget
-  }
-
-  get preview() {
-    // get preview image
-    return this.previewTarget
-  }
-
-  get setPreEditImage() {
-    default_image = this.previewTarget;
   }
 
   get fileCancelButton() {
